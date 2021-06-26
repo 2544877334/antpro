9d9d1aff4e1e4d6647eb011137d2ded15ba64a58
undefined <undefined>
2021-06-26 15:58:34 +0800

    chore: use vite-plugin-components

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,5 @@
 import 'ant-design-vue/dist/antd.less';
 import '@surely-vue/table/dist/index.css';
-import {
-  Layout,
-  Menu,
-  Row,
-  Col,
-  Card,
-  Form,
-  Dropdown,
-  Select,
-  Button,
-  Checkbox,
-  Tabs,
-  Tag,
-  Input,
-  DatePicker,
-  TimePicker,
-  Radio,
-  Tooltip,
-  Space,
-  Steps,
-  Divider,
-  Descriptions,
-  Alert,
-  Result,
-  Statistic,
-  Popconfirm,
-  Popover,
-  Table,
-  Avatar,
-  List,
-  Progress,
-  Switch,
-  Modal,
-  Rate,
-  ConfigProvider,
-  Empty,
-  Spin,
-  Drawer,
-  PageHeader,
-  Carousel,
-  BackTop,
-  Upload,
-  Badge,
-} from 'ant-design-vue';
 import SurelyTable from '@surely-vue/table';
 import { createApp, Plugin } from 'vue';
 import router from './router';
@@ -63,49 +19,7 @@ app
   .use(router)
   .use(locale as any)
   .use(store)
-  .use(Layout)
-  .use(Menu)
-  .use(Row)
-  .use(Col)
-  .use(Card)
-  .use(Form)
-  .use(Dropdown)
-  .use(Select)
-  .use(Button)
-  .use(Checkbox)
-  .use(Tabs)
-  .use(Tag)
-  .use(Input)
-  .use(DatePicker)
-  .use(TimePicker)
-  .use(Radio)
-  .use(Tooltip)
-  .use(Space)
-  .use(Steps)
-  .use(Divider)
-  .use(Descriptions)
-  .use(Alert)
-  .use(Result)
-  .use(Statistic)
-  .use(Popconfirm)
-  .use(Popover)
-  .use(Table)
-  .use(Avatar)
-  .use(List)
-  .use(Progress)
-  .use(Switch)
-  .use(Modal)
-  .use(Rate)
-  .use(ConfigProvider)
-  .use(Empty)
-  .use(Spin)
-  .use(Drawer)
-  .use(PageHeader)
   .use(ProProvider)
-  .use(Badge)
-  .use(Carousel)
-  .use(BackTop)
-  .use(Upload)
   .use(SurelyTable as Plugin)
   .component(PageContainer.name, PageContainer)
   .component(TransformVnode.name, TransformVnode)
