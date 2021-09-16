4e5d9be2c577cfb101182f0f25f22368be086746
undefined <undefined>
2021-09-16 15:05:20 +0800

    remove big-table-list

--- src/router/index.ts
+++ src/router/index.ts
@@ -95,13 +95,6 @@ export const routes: MenuDataItem[] = [
         redirect: '/list/table-list',
         children: [
           {
-            path: '/list/big-table-list',
-            name: 'big-table-list',
-            meta: { title: 'pages.list.bigtablelist.title' },
-            component: (): Component =>
-              import(/* webpackChunkName: "list" */ '@/views/list/big-table-list/index.vue'),
-          },
-          {
             path: '/list/table-list',
             name: 'table-list',
             meta: { title: 'pages.list.tablelist.title' },
